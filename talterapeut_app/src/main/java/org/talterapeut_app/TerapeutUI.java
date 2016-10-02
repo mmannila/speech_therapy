598c1b43d5151c1b128066e551b5b2ad2914749c
hsirvio <hsirvio@abo.fi>
2016-10-02 20:49:30 +0300

    Implemented basic functionality of the ImageFolder / Browser -button.

--- talterapeut_app/src/main/java/org/talterapeut_app/TerapeutUI.java
+++ talterapeut_app/src/main/java/org/talterapeut_app/TerapeutUI.java
@@ -33,43 +33,44 @@ import org.talterapeut_app.model.ImageLoader;
 @Theme("mytheme")
 //@SuppressWarnings("serial")
 public class TerapeutUI extends UI {
-	
-	int phrase_length = 3;	// the current phrase length (or no. of words)
-	ArrayList correct_order = new ArrayList(); // stores the correct order of the current phrase
+
+    final private String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+    int phrase_length = 3;	// the current phrase length (or no. of words)
+    ArrayList correct_order = new ArrayList(); // stores the correct order of the current phrase
     ArrayList<Image> setOfImages = new ArrayList<Image>();
 
     final GridLayout gridLayout = new GridLayout(3, 3);
-	
+
     VerticalLayout wordSelectLayout = new VerticalLayout();
     Button folderButton;
     Button randomButton;
     Button resetButton;
-    
+
     VerticalLayout dragDropALayout;
     Label phraseLabel;
     DDCssLayout dragDropArea_A;
     DDCssLayout dragDropArea_B;
     Button playPhraseButton;
-    
-    
+
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-    	initWordLengthLayout();
-    	initWordFolder();
-    	initRandomButton();
-    	initDragDropLayouts();
-    	initSoundButton();
-    	initResetButton();
+        initWordLengthLayout();
+        initWordFolder();
+        initRandomButton();
+        initDragDropLayouts();
+        initSoundButton();
+        initResetButton();
 
         randomize();
 
         setupUI();
-        
+
     }
-    
+
     // adds all components to the main layout
     private void setupUI() {
-    	// inserts components in grid layout
+        // inserts components in grid layout
         gridLayout.addComponent(wordSelectLayout, 0, 0);
         gridLayout.addComponent(folderButton, 1, 0);
         gridLayout.addComponent(randomButton, 2, 0);
@@ -77,98 +78,98 @@ public class TerapeutUI extends UI {
         gridLayout.addComponent(playPhraseButton, 2, 1);
         gridLayout.addComponent(dragDropArea_B, 1, 2);
         gridLayout.addComponent(resetButton, 2, 2);
-        
+
         gridLayout.setComponentAlignment(folderButton, Alignment.TOP_CENTER);
-        
+
         gridLayout.setSizeFull();
         gridLayout.setMargin(true);
         gridLayout.setSpacing(true);
         setContent(gridLayout);
     }
- 
+
     // initializes the buttons used to set phrase length
     private void initWordLengthLayout() {
-    	wordSelectLayout = new VerticalLayout();
-    	
-    	// listener which changes the phrase length + the other components
-    	ClickListener setWordCount = new ClickListener() {
-    	    @Override
-    	    public void buttonClick(ClickEvent event) {
-    	        // fetch button's corresponding number via its description to change
-    	    	// the word count (phrase_length) + refresh other components
-    	    }
-    	  };
-    		
+        wordSelectLayout = new VerticalLayout();
+
+        // listener which changes the phrase length + the other components
+        ClickListener setWordCount = new ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                // fetch button's corresponding number via its description to change
+                // the word count (phrase_length) + refresh other components
+            }
+        };
+
         Button twoWordButton = new Button("Two Words");
         Button threeWordButton = new Button("Three Words");
         Button fourWordButton = new Button("Four Words");
-        
+
         // these descriptions are used to set the phrase length
         twoWordButton.setDescription("2");
         twoWordButton.setDescription("3");
         twoWordButton.setDescription("4");
-        
+
         // all buttons use the same listener
         twoWordButton.addClickListener(setWordCount);
         threeWordButton.addClickListener(setWordCount);
         fourWordButton.addClickListener(setWordCount);
-        
+
         twoWordButton.setWidth("100%");
         threeWordButton.setWidth("100%");
         fourWordButton.setWidth("100%");
-        
+
         wordSelectLayout.addComponent(twoWordButton);
         wordSelectLayout.addComponent(threeWordButton);
         wordSelectLayout.addComponent(fourWordButton);
     }
-    
-    
+
+
     // the word folder containing all words/images
     private void initWordFolder() {
-    	folderButton = new Button("Word Folder");
-    	folderButton.addClickListener( e -> {
-        	
-		});
+        folderButton = new Button("Word Folder");
+        folderButton.addClickListener( e -> {
+            promptImageSelector();
+        });
     }
-    
+
     // button used to randomize
     private void initRandomButton() {
-    	randomButton = new Button("Randomize");
-    	randomButton.addClickListener( e -> {
+        randomButton = new Button("Randomize");
+        randomButton.addClickListener( e -> {
             randomize();
-    		});
+        });
     }
-    
+
     // initializes the drag and drop layouts
     private void initDragDropLayouts() {
-    	dragDropALayout = new VerticalLayout();    	
-    	phraseLabel = new Label();
-    	
-    	dragDropArea_A = new DDCssLayout();
-    	dragDropArea_A.setSizeFull();
+        dragDropALayout = new VerticalLayout();
+        phraseLabel = new Label();
+
+        dragDropArea_A = new DDCssLayout();
+        dragDropArea_A.setSizeFull();
         dragDropArea_A.setDragMode(LayoutDragMode.CLONE);
         dragDropArea_A.setDropHandler(new DefaultCssLayoutDropHandler());
-    	
+
         dragDropArea_B = new DDCssLayout();
         dragDropArea_B.setSizeFull();
         dragDropArea_B.setDragMode(LayoutDragMode.CLONE);
         dragDropArea_B.setDropHandler(new DefaultCssLayoutDropHandler());
-        
+
         dragDropALayout.addComponent(dragDropArea_A);
-        dragDropALayout.addComponent(phraseLabel);   
+        dragDropALayout.addComponent(phraseLabel);
         dragDropALayout.setSizeFull();
     }
 
     // plays the phrase in DnD layout A
     private void initSoundButton() {
-    	playPhraseButton = new Button("Play Sound");
-    	playPhraseButton.addClickListener( e -> {
-        	int length = dragDropArea_A.getComponentCount();
-        	if (length > 0) {
-        		String tmp = dragDropArea_A.getComponent(0).getDescription();
-            	for (int i = 1; i < length; i++)
-            		tmp += " " + dragDropArea_A.getComponent(i).getDescription();
-            	phraseLabel.setValue(tmp);
+        playPhraseButton = new Button("Play Sound");
+        playPhraseButton.addClickListener( e -> {
+            int length = dragDropArea_A.getComponentCount();
+            if (length > 0) {
+                String tmp = dragDropArea_A.getComponent(0).getDescription();
+                for (int i = 1; i < length; i++)
+                    tmp += " " + dragDropArea_A.getComponent(i).getDescription();
+                phraseLabel.setValue(tmp);
 
                 // test if the answer was correct or not
                 if (Objects.equals(tmp,"Subject Verb Object")) {
@@ -178,25 +179,25 @@ public class TerapeutUI extends UI {
                     new Notification("Incorrect").show(Page.getCurrent());
                 }
 
-        	}
-        	else
-        		phraseLabel.setValue("This DnD layout is empty!");
+            }
+            else
+                phraseLabel.setValue("This DnD layout is empty!");
         });
     }
-    
+
     // button which calls the reset method
     private void initResetButton() {
-    	resetButton = new Button("Reset");
-    	resetButton.addClickListener( e -> {
-    			reset();
-    		});
+        resetButton = new Button("Reset");
+        resetButton.addClickListener( e -> {
+            reset();
+        });
     }
-    
+
     // resets the DnD layouts and the phrase label
     private void reset() {
         phraseLabel.setValue("");
-    	dragDropArea_A.removeAllComponents();
-    	dragDropArea_B.removeAllComponents();
+        dragDropArea_A.removeAllComponents();
+        dragDropArea_B.removeAllComponents();
 
         for (int i = 0; i < 3; i++) {
             Image image = setOfImages.get(i);
@@ -206,10 +207,67 @@ public class TerapeutUI extends UI {
         }
     }
 
+    // prompt for the selection of images.
+    // TO DO: Split into modules
+    private void promptImageSelector() {
+
+        Window window = new Window("Image Selection");
+        window.setModal(true);
+        window.setWidth(300.0f, Unit.PIXELS);
+        window.setResizable(false);
+        window.center();
+
+        VerticalLayout folderWindowLayout = new VerticalLayout();
+        folderWindowLayout.setMargin(true);
+
+        window.setContent(folderWindowLayout);
+
+        Button subjectButton = new Button("Subject");
+        subjectButton.setWidth("100%");
+        folderWindowLayout.addComponent(subjectButton);
+
+        Panel subjPanel = new Panel();
+        subjPanel.setHeight(130.0f, Unit.PIXELS);
+        subjPanel.setVisible(false);
+
+        VerticalLayout subjWindowLayout = new VerticalLayout();
+        subjPanel.setContent(subjWindowLayout);
+        folderWindowLayout.addComponent(subjPanel);
+
+        // WORK IN PROGRESS
+        // Populate VerticalLayout within Panel (with scrollbar)
+        // Clear layout and disable Panel on re-click
+        subjectButton.addClickListener( e -> {
+
+            if (!subjPanel.isVisible()) {
+                ArrayList<Image> subjectArrayList = ImageLoader.loadImages(basepath + "/WEB-INF/subjekt/");
+                subjPanel.setVisible(true);
+
+                for (Image img : subjectArrayList) {
+                    subjWindowLayout.addComponent(img);
+                    subjWindowLayout.setComponentAlignment(img, Alignment.MIDDLE_CENTER);
+                }
+            } else {
+                subjWindowLayout.removeAllComponents();
+                subjPanel.setVisible(false);
+            }
+        });
+
+        Button verbButton = new Button("Verb");
+        verbButton.setWidth("100%");
+        folderWindowLayout.addComponent(verbButton);
+
+
+        getUI().getCurrent().addWindow(window);
+
+    }
+
+
+
     // Loads new set of images
     private void randomize() {
         phraseLabel.setValue("");
-        String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+
         int randomIndex;
         setOfImages.clear();
 
