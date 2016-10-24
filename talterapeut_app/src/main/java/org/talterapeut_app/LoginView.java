8b0baa7b869d84b698597f638e4788d938158438
Daniel Vainio <vainio.daniel@gmail.com>
2016-10-24 19:16:21 +0300

    UI and database update.

--- talterapeut_app/src/main/java/org/talterapeut_app/LoginView.java
+++ talterapeut_app/src/main/java/org/talterapeut_app/LoginView.java
@@ -2,10 +2,12 @@ package org.talterapeut_app;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.Page;
 import com.vaadin.ui.*;
 import org.talterapeut_app.loginview.LoginDAO;
 
 import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Loginview
@@ -20,28 +22,30 @@ public class LoginView extends VerticalLayout implements View {
 
     public LoginView() {
 
+        Label lb = new Label("Sign in");
+
         VerticalLayout mainLayout = new VerticalLayout();
         HorizontalLayout buttonLayout = new HorizontalLayout();
 
+        mainLayout.setWidth("400");
+
         mainLayout.setSpacing(true);
         mainLayout.setMargin(true);
-
         buttonLayout.setSpacing(true);
-        buttonLayout.setMargin(true);
 
         emailField = new TextField("E-mail or username:");
         emailField.setInputPrompt("name@email.com");
         emailField.setRequired(true);
-        emailField.setWidth("300");
+        emailField.setWidth("250");
 
         usernameField = new TextField("Username:");
         usernameField.setRequired(true);
-        usernameField.setWidth("300");
+        usernameField.setWidth("250");
         usernameField.setVisible(false);
 
         passwordField = new PasswordField("Password:");
         passwordField.setRequired(true);
-        passwordField.setWidth("300");
+        passwordField.setWidth("250");
 
         Button loginButton = new Button("Login");
         loginButton.addClickListener(login -> {
@@ -55,9 +59,12 @@ public class LoginView extends VerticalLayout implements View {
         });
 
         Button createUserButton = new Button("Create user");
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setVisible(false);
 
         Button registerButton = new Button("Register");
         registerButton.setVisible(false);
+
         registerButton.addClickListener(register -> {
             try {
                 Register();
@@ -65,25 +72,28 @@ public class LoginView extends VerticalLayout implements View {
                 e.printStackTrace();
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         });
 
         createUserButton.addClickListener(create -> {
             createUserButton.setVisible(false);
             registerButton.setVisible(true);
+            loginButton.setVisible(false);
+            cancelButton.setVisible(true);
             usernameField.setVisible(true);
             emailField.setCaption("E-mail:");
         });
 
-        buttonLayout.addComponents(loginButton, createUserButton, registerButton);
-        mainLayout.addComponents(emailField, usernameField, passwordField, buttonLayout);
+        cancelButton.addClickListener(cancel -> Page.getCurrent().setLocation("/"));
 
-        mainLayout.setComponentAlignment(buttonLayout, Alignment.MIDDLE_CENTER);
-        mainLayout.setComponentAlignment(emailField, Alignment.MIDDLE_CENTER);
-        mainLayout.setComponentAlignment(usernameField, Alignment.MIDDLE_CENTER);
-        mainLayout.setComponentAlignment(passwordField, Alignment.MIDDLE_CENTER);
+        buttonLayout.addComponents(loginButton, createUserButton, registerButton, cancelButton);
+        mainLayout.addComponents(lb, emailField, usernameField, passwordField, buttonLayout);
 
         addComponent(mainLayout);
+        setComponentAlignment(mainLayout, Alignment.MIDDLE_CENTER);
+        setResponsive(true);
     }
 
     private void Login() throws SQLException, ClassNotFoundException {
@@ -96,10 +106,12 @@ public class LoginView extends VerticalLayout implements View {
         }
     }
 
-    private void Register() throws SQLException, ClassNotFoundException {
+    private void Register() throws SQLException, ClassNotFoundException, InterruptedException {
         login = new LoginDAO(emailField.getValue(), usernameField.getValue(), passwordField.getValue());
 
         if (login.Validation() && login.CreateUser()) {
+            TimeUnit.SECONDS.sleep(1);
+            Page.getCurrent().setLocation("/");
             Notification.show("Successful registration.");
 
         } else {
