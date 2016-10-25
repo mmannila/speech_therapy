a92b8aa37be3a3d5d48d8d1bdd5ca299fb429cf3
Daniel Vainio <vainio.daniel@gmail.com>
2016-10-25 16:35:18 +0300

    Prevent user from accessing profile view without logging in.

--- talterapeut_app/src/main/java/org/talterapeut_app/ProfileView.java
+++ talterapeut_app/src/main/java/org/talterapeut_app/ProfileView.java
@@ -4,6 +4,7 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 public class ProfileView extends VerticalLayout implements View {
@@ -27,8 +28,9 @@ public class ProfileView extends VerticalLayout implements View {
 
     @Override
     public void enter(ViewChangeEvent event) {
-        // TODO Auto-generated method stub
 
+        if (UI.getCurrent().getSession().getAttribute("email") == null) {
+            TerapeutUI.navigator.navigateTo(TerapeutUI.LOGINVIEW);
+        }
     }
-
 }
